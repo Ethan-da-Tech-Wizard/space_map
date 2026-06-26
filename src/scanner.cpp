2ca178bd9c4b552b4ab3977bc9b529997aa0efc6
Ethan <ethan@example.com>
2026-06-25 22:11:15 -0700

    Define S_ISDIR compatibility macro for Windows MSVC

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
+
+#ifndef S_ISDIR
+#define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 #else
